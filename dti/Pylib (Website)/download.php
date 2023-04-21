14cbdf79f81731e3242f822c3854b1b082ed076c
Eshparsi <112681516+Eshparsi@users.noreply.github.com>
2023-04-21 18:07:12 +0530

    Add files via upload

---
+++ dti/Pylib (Website)/download.php
@@ -0,0 +1,12 @@
+<?php
+$file =$_Get['file'].".zip";
+
+header("content-disposition:attachment; filename=".urlencode($file));
+
+$fb =fopen($file,"r");
+while(!feof($fb)){
+    echo fread($fb,8192);
+    flush();
+}
+fclose($fb);
+?>
